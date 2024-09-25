665ab515e3074f1e3c1569ac4ffd425f070369b6
Christian Alejandro Flores Gonzalez <christianflores57@gmail.com>
2024-09-25 16:28:51 +0800

    Changes to the plot JCMT images
    I have substantially changed this file so it can 1) prepare the moment maps, it runs .sh script to get the kappa functions of resampling and such, 2) it runs BTS to get the moment maps, 3) it can plot the maps and save as figures

--- moment_preparation.sh
+++ moment_preparation.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
-
+export STARLINK_DIR=/Users/christianflores/Programs/star-2023A 
+source $STARLINK_DIR/etc/profile 
+cd /Users/christianflores/Documents/work/Astronomy_data/JCMT/M22BH10B/V347Aur/C18O/reduced/
 kappa
-
-INPUTNAME="ga20220820_15_1_0p20bin001_new"
-
+INPUTNAME="ga20220818_48_1_0p20bin001"
 SOURCE="V347_Aur"
-#MOLEC="C18O"
-MOLEC="HCO+"
-RESA="_resamp"
+MOLEC="C18O"
+RESA="_resampled"
 SOURCENAME=$SOURCE$MOLEC
 RESAMP=$SOURCE$MOLEC$RESA
-
 cdiv in=$INPUTNAME.sdf scalar=0.63 out=$SOURCENAME.sdf
-
 setunits $SOURCENAME.sdf units=\"K km/s \"
-
 sqorst in=$SOURCENAME.sdf out=$RESAMP.sdf factors="[6,6,1] conserve"
-
 convert
 ndf2fits in=$RESAMP.sdf out=$RESAMP.fits
+mv $RESAMP.sdf /Users/christianflores/Documents/GitHub/JCMT/sdf_and_fits/.
+mv $RESAMP.fits /Users/christianflores/Documents/GitHub/JCMT/sdf_and_fits/.
+cd /Users/christianflores/Documents/GitHub/JCMT
--- plot_JCMT_images.py
+++ plot_JCMT_images.py
@@ -1,12 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Ellipse
-import sys
-import scipy.ndimage.interpolation as interpol
 import os
+import subprocess
+import shutil
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-from astropy.io import fits as pyfits
-import matplotlib.colors as colors
 
 #Astropy modules to deal with coordinates
 from astropy.wcs import WCS
@@ -18,6 +15,7 @@ from astropy.coordinates import SkyCoord
 from astropy import units as u
 from astropy.visualization.wcsaxes import SphericalCircle
 
+import BTS
 
 class DataAnalysis:
     """
@@ -55,6 +53,8 @@ class DataAnalysis:
 
         self.cdelt_ra = self.header['CDELT1'] * 3600
 
+
+
         try:
             self.nz = self.header['NAXIS3']
             self.vel = self.get_vel(self.header)
@@ -62,8 +62,10 @@ class DataAnalysis:
             if (dv < 0):
                 dv = dv * -1
 
-            self.molecule = self.header['MOLECULE']
+            self.molecule = self.header['MOLECULE'].split(' ')[0].replace('-', '')
 
+            s_name = self.header['OBJECT'].strip()
+            self.source_name = s_name.replace(' ', '_')
 
         except:
             print('This is a 2D image')
@@ -91,6 +93,136 @@ class DataAnalysis:
             print("The CTYPE3 variable in the fitsfile header does not start with F for frequency or V for velocity")
             return
 
+def create_shell_script_moment_maps(path_to_folder,sdf_name,source_name,molec):
+    """
+    Create the shell script that runs all the necessary tasks in kappa
+    to go from antenna temperature to Main Beam temperature
+    change the units to Km/s
+    re-sample the datacube
+
+    :param path_to_folder: folder of the .sdf files starting with PROGRAM NUMBER
+    :param sdf_name: name of the reduced data (NO .sdf extension)
+    :param source_name: source name
+    :param molec: molecule name - either HCO+ or C18O
+    :return:
+    """
+    if '.sdf' in sdf_name:
+        sdf_name = sdf_name.strip('.sdf')
+
+    file = open(os.path.join('.',"moment_map_preparation.sh"), "w")
+    file.write('#!/usr/bin/env bash \n')
+    file.write('export STARLINK_DIR=/Users/christianflores/Programs/star-2023A \n')
+    file.write('source $STARLINK_DIR/etc/profile  \n')
+    file.write('cd /Users/christianflores/Documents/work/Astronomy_data/JCMT/'+path_to_folder+'\n')
+    file.write('kappa \n')
+    file.write('INPUTNAME="'+sdf_name+'" \n')
+    file.write('SOURCE="'+source_name+'" \n')
+    file.write('MOLEC="'+molec+'" \n')
+    # file.write('RESA="_resampled" \n')
+    file.write('SOURCENAME=$SOURCE"_"$MOLEC \n')
+    # file.write('RESAMP=$SOURCE"_"$MOLEC$RESA \n')
+    file.write('RESAMP=$SOURCE"_"$MOLEC$ \n')
+    file.write('cdiv in=$INPUTNAME.sdf scalar=0.63 out=$SOURCENAME.sdf \n')
+    file.write('setunits $SOURCENAME.sdf units=\"K km/s \" \n')
+    file.write('sqorst in=$SOURCENAME.sdf out=$RESAMP.sdf factors="[6,6,1] conserve" \n')
+    file.write('convert \n')
+    file.write('ndf2fits in=$RESAMP.sdf out=$RESAMP.fits \n')
+    file.write('mv $RESAMP.sdf /Users/christianflores/Documents/GitHub/JCMT/sdf_and_fits/'+source_name+ '\n')
+    file.write('mv $RESAMP.fits /Users/christianflores/Documents/GitHub/JCMT/sdf_and_fits/'+source_name+ ' \n')
+    file.write('cd /Users/christianflores/Documents/GitHub/JCMT \n')
+    file.close()
+
+def run_moment_map_shell_script(path_to_folder):
+    """
+    Run the script moment_map_preparation.sh
+    :param path_to_folder:
+    :return:
+    """
+    subprocess.check_call(['bash',os.path.join(path_to_folder,"moment_map_preparation.sh")], cwd=path_to_folder)
+
+
+def create_moment_masking_parameterfile(folder_file, fits_file_name,cube_param_name='Fit_cube.param'):
+    '''
+    Copy the parameter file needed to run BTS and create moment maps
+    Modify the files themselves so they have the appropriate input data
+    source
+    folder_file: the directory of the folder
+    fits_file_name: name of the fits file datacube that wants to be used
+    for moment map creating
+    cube_param_name: default is Fit_cube.param in the same directory as this file
+    '''
+
+    data_cube = DataAnalysis(folder_file, fits_file_name + '.fits')
+    molecule = data_cube.molecule
+    source_name =data_cube.source_name
+
+    ### copying the file
+    moment_param_filename = source_name + '_' + molecule + '_moments.param'  ## Name of the cube.param file
+
+    full_path_moment_param_filename = os.path.join(folder_file, moment_param_filename)  ## full name including path
+    copy_text_files(cube_param_name, full_path_moment_param_filename)
+
+    ### modifying the file
+    new_fits_path = os.path.join(folder_file, fits_file_name+'.fits')
+    replace_line(full_path_moment_param_filename, 'data_in_file_name', new_fits_path)
+    save_folder = os.path.join('moment_maps', source_name)
+    output_base = os.path.join(save_folder, source_name + '_' + molecule)
+    replace_line(full_path_moment_param_filename, 'output_base', output_base)
+
+    ### make directory to save file if it does not exist
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+
+    return full_path_moment_param_filename
+
+
+def copy_text_files(source, destination):
+    '''
+    # Copy the content of the moment masking parameter to the folder of the fits files
+    # source to destination is the folder
+    '''
+
+    try:
+        shutil.copyfile(source, destination)
+        print("File copied successfully.")
+
+    # If source and destination are same
+    except shutil.SameFileError:
+        print("Source and destination represents the same file.")
+
+    # If destination is a directory.
+    except IsADirectoryError:
+        print("Destination is a directory.")
+
+    # If there is any permission issue
+    except PermissionError:
+        print("Permission denied.")
+
+    # For other errors
+    except:
+        print("Error occurred while copying file.")
+
+
+def replace_line(file_name, key_text, new_text):
+    lines = open(file_name, 'r').readlines()
+    for count, line in enumerate(lines):
+        # for line in lines:
+        if key_text in line:
+            text_to_change = line.split()[2]
+            replaced_line = line.replace(text_to_change, new_text)
+            line_num = count
+            # print(text_to_change)
+    lines[line_num] = replaced_line
+    out = open(file_name, 'w')
+    out.writelines(lines)
+    out.close()
+
+def run_BTS(param_file):
+    # Read in the relevant parameter file
+    param = BTS.read_parameters(param_file)
+    print(param)
+    # Run the function to make the moments using the moment-masking technique
+    BTS.make_moments(param)
 
 def integrated_intensity(path, filename):
     '''
@@ -156,7 +288,7 @@ def plot_average_spectrum(path,filename,save=False):
     plt.show()
 
 
-def plot_moment_zero_map(path,filename,save=False):
+def plot_moment_zero_map(filename,source_name,save=False):
     '''
     Create moment maps using the python package bettermoments.
     Currently only moment 0 and 8 work. Some unknown issues with the velocity
@@ -177,16 +309,17 @@ def plot_moment_zero_map(path,filename,save=False):
     '''
 
 
-    data_cube = DataAnalysis(path, filename+'.fits')
-    moment_0 = DataAnalysis(path, filename+'_mom0.fits')
+    data_cube = DataAnalysis(os.path.join('sdf_and_fits',source_name), filename+'.fits')
+    moment_0 = DataAnalysis(os.path.join('moment_maps',source_name), filename+'_mom0.fits')
 
+    print('molecule ',data_cube.molecule)
     ### Here I can go from sky position to pixel coordinates
 
     image_mom_0 = moment_0.ppv_data
 
     if 'HCO+' in data_cube.molecule:
         aperture_radius = 7.05
-    elif 'C18O ' in data_cube.molecule:
+    elif data_cube.molecule == 'C18O':
         aperture_radius = 7.635
     else:
         raise Exception("Sorry, I need to calculate such aperture radius")
@@ -200,7 +333,7 @@ def plot_moment_zero_map(path,filename,save=False):
 
     ## Moment zero
     fig1 = plt.subplot(projection=moment_0.wcs)
-    mom0_im = fig1.imshow(image_mom_0, cmap="viridis", origin='lower',vmax=0.7)
+    mom0_im = fig1.imshow(image_mom_0, cmap="viridis", origin='lower')#,vmax=0.7)
     # divider = make_axes_locatable(fig1)
     # cax = divider.append_axes("right", size="5%", pad=0.05)
     cbar = plt.colorbar(mom0_im, fraction=0.048, pad=0.04, label='Integrated Intensity (K * km/s)')
@@ -218,12 +351,25 @@ def plot_moment_zero_map(path,filename,save=False):
         plt.savefig(os.path.join('Figures',filename), bbox_inches='tight')
     plt.show()
 
+
 if __name__ == "__main__":
 
-    path = '.'
-    filename='V347_AurHCO+_resamp'
-    skycoord_object = SkyCoord('04 56 57.0 +51 30 50.88', unit=(u.hourangle, u.deg))
+    source_name = 'V347_Aur'
+    molecule ='HCO+'
+    ### Get the shell script for moment map preparation
+    # path_to_folder='M22BH10B/V347Aur/C18O/reduced/'
+    # create_shell_script_moment_maps(path_to_folder,sdf_name='ga20220818_48_1_0p20bin001',source_name=source_name,molec='C18O')
+    # run_moment_map_shell_script(path_to_folder='.')
 
-    plot_moment_zero_map(path, filename,save=True)
+    #### run the BTS to create moment maps
+    # folder_file='sdf_and_fits/V347_Aur/'
+    # fits_file_name='V347_Aur_HCO+'
+    # BTS_param_file = create_moment_masking_parameterfile(folder_file, fits_file_name)
+    # run_BTS(BTS_param_file)
+
+    ### Plot the maps
+    filename='V347_Aur_HCO+'
+    skycoord_object = SkyCoord('04 56 57.0 +51 30 50.88', unit=(u.hourangle, u.deg))
+    plot_moment_zero_map(filename,source_name=source_name,save=True)
     # plot_average_spectrum(path, filename,save=True)
     # make_average_spectrum_data(path, filename)
